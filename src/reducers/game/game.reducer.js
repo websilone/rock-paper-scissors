acc943c6de069e67a960d10e531a4cd63b461a13
Boris LOUBOFF <boris@louboff.com>
2017-11-01 20:34:14 +0100

    Play action and reducer

--- src/reducers/game/game.reducer.js
+++ src/reducers/game/game.reducer.js
@@ -6,11 +6,25 @@ import { GAME_STATUS_START } from '../../constants/gameStatus.constants';
 
 export const types = {
   INIT: 'GAME/INIT',
+  PLAY: 'GAME/PLAY',
+};
+
+export const PLAYERS = {
+  PLAYER1: 'player1',
+  PLAYER2: 'player2',
+};
+
+const DEFAULT_PLAYER = {
+  selectedShape: null,
 };
 
 export const INITIAL_STATE = {
   availableShapes : [],
   gameStatus: null,
+  players: {
+    [PLAYERS.PLAYER1]: { ...DEFAULT_PLAYER },
+    [PLAYERS.PLAYER2]: { ...DEFAULT_PLAYER },
+  },
 };
 
 export default createReducer(INITIAL_STATE, {
@@ -19,10 +33,24 @@ export default createReducer(INITIAL_STATE, {
       ...state,
       availableShapes: utils.getAvailableShapes(config),
       gameStatus: GAME_STATUS_START,
+      player1: {...DEFAULT_PLAYER},
+      player2: {...DEFAULT_PLAYER},
     };
+  },
+
+  [types.PLAY] (state, { payload: { player, shape } }) {
+    if (!state.players[player] || !utils.getAvailableShapes(config).includes(shape)) {
+      return state;
+    }
+    
+    return {
+      ...state,
+      players: { ...state.players, [player]: { selectedShape: shape } },
+    }
   }
 });
 
 export const actions = {
   init: () => ({ type: types.INIT}),
+  play: (player, shape) => ({ type: types.PLAY, payload: { player, shape }}),
 };
--- src/reducers/game/game.reducer.spec.js
+++ src/reducers/game/game.reducer.spec.js
@@ -1,9 +1,12 @@
 import sinon from 'sinon';
 
+import config from '../../config';
 import { GAME_STATUS_START } from '../../constants/gameStatus.constants';
 
 import gameUtils from './gameUtils';
-import reducer, { types, INITIAL_STATE } from './game.reducer';
+import reducer, { types, INITIAL_STATE, PLAYERS, actions } from './game.reducer';
+
+const { ROCK_SHAPE } = config;
 
 describe('Game reducer', () => {
   const sandbox = sinon.sandbox.create();
@@ -13,19 +16,33 @@ describe('Game reducer', () => {
   });
  
   describe('Initial state', () => {
+    let state;
+    
+    beforeEach(() => {
+      state = reducer();
+    });
+
     describe('availableShapes', () => {
       it('should be an empty array', () => {
-        const state = reducer();
-
         expect(state.availableShapes).toEqual([]);
       });
     });
 
     describe('gameStatus', () => {
-      it('should be undefined', () => {
-        const state = reducer();
-
+      it('should be null', () => {
         expect(state.gameStatus).toBe(null);
+      });
+    });
+
+    describe(`${PLAYERS.PLAYER1}`, () => {
+      it('should be have a selectedShape property set to null', () => {
+        expect(state.players[PLAYERS.PLAYER1].selectedShape).toBe(null);
+      });
+    });
+
+    describe(`${PLAYERS.PLAYER2}`, () => {
+      it('should be have a selectedShape property set to null', () => {
+        expect(state.players[PLAYERS.PLAYER2].selectedShape).toBe(null);
       });
     });
   });
@@ -45,6 +62,77 @@ describe('Game reducer', () => {
 
     it('should set the gameStatus to START', () => {
       expect(state.gameStatus).toEqual(GAME_STATUS_START);
+    });
+  });
+
+  describe('PLAY', () => {
+    describe('The player does not exist', () => {
+      it('should return the unchanged state', () => {
+        const state = reducer(INITIAL_STATE, { type: types.PLAY, payload: { player: 'toto', shape: ROCK_SHAPE }});
+
+        expect(state).toEqual(INITIAL_STATE);
+      });
+    });
+
+    describe('The shape in not one of the available ones', () => {
+      it('should return the unchanged state', () => {
+        const state = reducer(INITIAL_STATE, { type: types.PLAY, payload: { player: PLAYERS.PLAYER1, shape: 'someShape' } });
+
+        expect(state).toEqual(INITIAL_STATE);
+      });
+    });
+
+    describe('with correct player and shape', () => {
+      it('should update the selectedShape for the given player', () => {
+        const state = reducer(INITIAL_STATE, { type: types.PLAY, payload: { player: PLAYERS.PLAYER1, shape: ROCK_SHAPE } });
+
+        expect(state.players[PLAYERS.PLAYER1].selectedShape).toBe(ROCK_SHAPE);
+        expect(state.players[PLAYERS.PLAYER2].selectedShape).toBe(null);
+      });
+    });
+  });
+
+  describe('Actions', () => {
+    describe('play', () => {
+      it('should be a function', () => {
+        expect(actions.play).toBeInstanceOf(Function);
+      });
+
+      it('should return an object with a type PLAY', () => {
+        const action = actions.play();
+
+        expect(action.type).toBe(types.PLAY);
+      });
+
+      it('should return an object with a payload', () => {
+        const action = actions.play();
+
+        expect(action.payload).toBeDefined();
+      });
+
+      it('should return the correct payload according to the params', () => {
+        const action = actions.play('player', 'shape');
+
+        expect(action.payload).toEqual({ player: 'player', shape: 'shape' });
+      });
+    });
+
+    describe('init', () => {
+      it('should be a function', () => {
+        expect(actions.init).toBeInstanceOf(Function);
+      });
+
+      it('should return an object with a type INIT', () => {
+        const action = actions.init();
+
+        expect(action.type).toBe(types.INIT);
+      });
+
+      it('should return an object without a payload', () => {
+        const action = actions.init();
+
+        expect(action.payload).toBeUndefined();
+      });
     });
   });
 });
