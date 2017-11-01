87d2216bb4e50046fbf79899cb322021babc0426
Boris LOUBOFF <boris@louboff.com>
2017-11-01 22:09:00 +0100

    Using I18N data for App header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import I18N from '../../i18n';
+
 const Header = () => (
   <section className="hero is-info">
     <div className="hero-body">
       <div className="container">
         <h1 className="title">
-          Rock, Paper, Scissors
+          { I18N.headerTitle }
         </h1>
       </div>
     </div>
