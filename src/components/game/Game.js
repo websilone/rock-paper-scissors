26e4b9e747d91b8199dab90577b5b33aa49e851c
Boris LOUBOFF <boris@louboff.com>
2017-11-01 18:36:31 +0100

    Render the shapes for both players

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -8,14 +8,54 @@ class Game extends Component {
     this.props.init();
   }
 
-  render() {
+  renderShapes() {
     const { shapes } = this.props;
 
     return (
       <div>
-        { shapes.map((shape, idx) => {
-          return (<Shape key={idx} shape={shape} />);
-        }) }
+        {
+          shapes.map((shape, idx) => {
+            return (<Shape key={idx} shape={shape} />);
+          })
+        }
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <section className="hero">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns">
+                <div className="column">
+                  <p className="has-text-centered">
+                    <span className="tag is-large is-rounded">
+                      <span className="icon is-small" style={{ marginRight: '1em' }}><i className="fa fa-user" /></span>You
+                    </span>
+                  </p>
+                  <br />
+                  {this.renderShapes()}
+                </div>
+
+                <div className="column">
+                  
+                </div>
+
+                <div className="column">
+                  <p className="has-text-centered">
+                    <span className="tag is-large is-rounded">
+                      <span className="icon is-small" style={{ marginRight: '1em' }}><i className="fa fa-desktop" /></span>Computer
+                    </span>
+                  </p>
+                  <br />
+                  {this.renderShapes()}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
--- src/components/game/Game.spec.js
+++ src/components/game/Game.spec.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
+import config from '../../config';
 import Game from './Game';
+import Shape from '../shape/Shape';
+
+const { ROCK_SHAPE, PAPER_SHAPE } = config;
 
 describe('Game component', () => {
   it('should call the init prop on componentDidMount', () => {
@@ -10,5 +14,14 @@ describe('Game component', () => {
     shallow(<Game init={initStub} shapes={[]} />);
 
     expect(initStub.calledOnce).toBe(true);
+  });
+
+  describe('shapes rendering', () => {
+    it('should render 2 sets of available shapes', () => {
+      const shapes = [ROCK_SHAPE, PAPER_SHAPE];
+      const wrapper = shallow(<Game init={() => {}} shapes={shapes} />);
+
+      expect(wrapper.find(Shape)).toHaveLength(shapes.length*2);
+    });
   });
 });
